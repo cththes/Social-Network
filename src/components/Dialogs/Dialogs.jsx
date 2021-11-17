2309c5ec9784c9d9a4e5cb2f466d0b878389f28b
cth <cththes@gmail.com>
2021-11-17 18:15:31 +0300

    Unit 21 Dialogs Layout

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import styles from './Dialogs.module.css'
-import Message from './Message/Message'
-import User from './User/User'
+import React from "react";
+import styles from "./Dialogs.module.css";
+import Message from "./Message/Message";
+import User from "./User/User";
 
 const Dialogs = () => {
-   return(
-      <div className = {styles.item}>
-         <User />
-         <Message />
+  return (
+    <div className={styles.dialogs}>
+      <div className={styles.dialogsItems}>
+        <div className={styles.dialog}>cth</div>
+        <div className={styles.dialog}>fv</div>
+        <div className={styles.dialog}>Valentin Oblomov</div>
       </div>
-   )
-}
+      <User />
+      <div className="messages">
+        <Message />
+        <Message />
+        <Message />
+      </div>
+    </div>
+  );
+};
 
-export default Dialogs
+export default Dialogs;
