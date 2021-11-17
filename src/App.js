64276c5db11e234516615fc8adf6f263e761f637
cth <cththes@gmail.com>
2021-11-17 05:28:55 +0300

    Unit 19 BrowserRouter, Route, Routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { Route, Routes } from "react-router";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 
-
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
       <Navbar />
-      {/*<Profile />*/}
       <div className="app-wrapper-content">
-        <Dialogs />
+        <Routes>
+          <Route element={<Profile />} path="/profile" />
+          <Route element={<Dialogs />} path="/dialogs" />
+          <Route path="/news" />
+          <Route path="/music" />
+          <Route path="/settings" />
+        </Routes>
       </div>
     </div>
   );
