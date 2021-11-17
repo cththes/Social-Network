ede27f58298da81982f73bde562daa66a0751730
cth <cththes@gmail.com>
2021-11-18 00:13:46 +0300

    Unit 31 onClick, ref, VirtualDOM

--- src/components/Dialogs/User/User.jsx
+++ src/components/Dialogs/User/User.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import styles from "./User.module.css";
 
 const User = () => {
+  let newMessageElement = React.createRef();
+
+  let sendMessage = () => {
+    let text = newMessageElement.current.value;
+    alert(text);
+  };
   return (
     <div className={styles.item}>
       <img
         className={styles.avatar}
         src="https://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/85/858258a36b14983df2fcab7dd840dcc2bb13aa2e_full.jpg"
       />
-      <div className = {styles.message_area}>
-        <textarea placeholder='Введите сообщение: '></textarea>
+      <div className={styles.message_area}>
+        <textarea
+          ref={newMessageElement}
+          placeholder="Введите сообщение: "
+        ></textarea>
         <div>
-          <button>Отправить</button>
+          <button onClick={sendMessage}>Отправить</button>
         </div>
       </div>
     </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,13 +6,20 @@ const MyPosts = (props) => {
   let postsElements = props.posts.map((posts) => (
     <Post id={posts.id} message={posts.message} />
   ));
+
+  let newPostElement = React.createRef();
+
+  let addPost = ()=>{
+    let text = newPostElement.current.value
+    alert(text)
+  };
   return (
     <div>
       My Posts:
       <div className={styles.post_area}>
-        <textarea placeholder="Введите текст"></textarea>
+        <textarea ref={newPostElement} placeholder="Введите текст"></textarea>
         <div>
-          <button>Add Post</button>
+          <button onClick={addPost}>Add Post</button>
         </div>
       </div>
       <div>{postsElements}</div>
