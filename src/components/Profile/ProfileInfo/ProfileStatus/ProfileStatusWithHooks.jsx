97bd30e8e0df165f157ed6402e607f5d7f658fcc
cth <cththes@gmail.com>
2022-05-15 17:16:39 +0300

    85 hook, useEffect

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./ProfileStatus.module.css";
 
 const ProfileStatusWithHooks = (props) => {
-  
   let [editMode, setEditMode] = useState(false);
   let [status, setStatus] = useState(props.status);
+
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
 
   const activateEditMode = () => {
     setEditMode(true);
@@ -12,8 +15,8 @@ const ProfileStatusWithHooks = (props) => {
 
   const deactivateEditMode = () => {
     setEditMode(false);
-    props.updateStatus(status)
-  }
+    props.updateStatus(status);
+  };
 
   const onStatusChange = (e) => {
     setStatus(e.currentTarget.value);
@@ -23,7 +26,9 @@ const ProfileStatusWithHooks = (props) => {
     <div className={styles.status}>
       {!editMode && (
         <div>
-          <span onDoubleClick={activateEditMode}>{props.status || "---------"}</span>
+          <span onDoubleClick={activateEditMode}>
+            {props.status || "---------"}
+          </span>
         </div>
       )}
       {editMode && (
